b499a3c58e4107d314265d30dfb7429196ee3b19
ridwansulaiman <ridwan.sulaimann@gmail.com>
2023-03-19 19:03:32 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/tflab-rdw/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "rdw"
-  bucket_prefix = "rdw"
+  #bucket = "rdw"
+  #bucket_prefix = "rdw"
 }
